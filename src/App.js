b8ba55f846ad604bc014adeb572adce89c7fb907
Guillaume Claret <dev@clarus.me>
2019-08-16 00:02:30 +0200

    Have four panels for JS, JS AST, Coq AST, Coq

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // @flow
 import React, {PureComponent} from 'react';
-import CoqOutput from './CoqOutput.js';
+import codeFrame from 'babel-code-frame';
+import {parse} from '@babel/parser';
+import * as Program from "./compiler/program.js";
+import Output from './Output.js';
 import './App.css';
 
 type Props = {};
@@ -18,15 +21,55 @@ export default class App extends PureComponent<Props, State> {
     this.setState({jsInput: event.currentTarget.value});
   };
 
+  getJsAst(jsInput: string): any | string {
+    try {
+      const ast = parse(jsInput, {
+        plugins: [
+          'flow',
+          'jsx',
+        ],
+        sourceType: 'module',
+      });
+
+      return ast;
+    } catch (error) {
+      const {loc} = error;
+
+      if (loc) {
+        return `${error.message}\n\n${codeFrame(jsInput, loc.line, loc.column)}`;
+      }
+
+      throw error;
+    }
+  }
+
+  getCoqAst(jsAst: any): any {
+    try {
+      return Program.compile(jsAst.program);
+    } catch (error) {
+      return `Error:\n${error.message}`;
+    }
+  }
+
   render() {
     const {jsInput} = this.state;
+    const jsAst = this.getJsAst(jsInput);
+    const coqAst = typeof jsAst !== "string" ? this.getCoqAst(jsAst) : "";
 
     return (
       <div>
-        <div className="split left">
+        <div className="split js-source">
           <textarea onChange={this.onChangeJsInput} value={jsInput} />
         </div>
-        <CoqOutput jsInput={jsInput} />
+        <div className="split js-ast">
+          <Output output={jsAst} />
+        </div>
+        <div className="split coq-ast">
+          <Output output={coqAst} />
+        </div>
+        <div className="split coq-source">
+          <Output output="..." />
+        </div>
       </div>
     );
   }
---
+++ src/Output.js
@@ -0,0 +1,14 @@
+// @flow
+import React, {PureComponent} from 'react';
+
+type Props = {
+  output: any | string,
+};
+
+export default class CoqOutput extends PureComponent<Props> {
+  render() {
+    const {output} = this.props;
+
+    return <code><pre>{typeof output !== "string" ? JSON.stringify(output, null, 2) : output}</pre></code>;
+  }
+}
