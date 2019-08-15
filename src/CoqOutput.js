dbfe5c7ad3e037ff4df4d0bd3780958291912edf
Guillaume Claret <dev@clarus.me>
2019-08-15 18:47:18 +0200

    Add parsing error messages

--- src/CoqOutput.js
+++ src/CoqOutput.js
@@ -1,18 +1,30 @@
 import React, {PureComponent} from 'react';
+import codeFrame from 'babel-code-frame';
 import {parse} from '@babel/parser';
 
 export default class CoqOutput extends PureComponent {
   getCoqOutput() {
     const {jsInput} = this.props;
-    const ast = parse(jsInput, {
-      plugins: [
-        'flow',
-        'jsx',
-      ],
-      sourceType: 'module',
-    });
 
-    return JSON.stringify(ast, null, 2);
+    try {
+      const ast = parse(jsInput, {
+        plugins: [
+          'flow',
+          'jsx',
+        ],
+        sourceType: 'module',
+      });
+
+      return JSON.stringify(ast, null, 2);
+    } catch (error) {
+      const {loc} = error;
+
+      if (loc) {
+        return `${error.message}\n\n${codeFrame(jsInput, loc.line, loc.column)}`;
+      }
+
+      throw error;
+    }
   }
 
   render() {
