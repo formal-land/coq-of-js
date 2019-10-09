ff8cf128169d8a578093a3a4ff60efb458d12662
Guillaume Claret <dev@clarus.me>
2019-10-09 16:16:00 +0200

    Add Jest tests

---
+++ src/compiler/index.js
@@ -0,0 +1,26 @@
+// @flow
+import {parse} from "@babel/parser";
+import doc from "prettier/doc.js";
+import * as Error from "./error.js";
+import * as Monad from "./monad.js";
+import * as Program from "./program.js";
+
+export function compileAndPrint(jsInput: string, printWidth: number): string {
+  const jsAst = parse(jsInput, {
+    plugins: ["flow", "jsx"],
+    sourceType: "module",
+  });
+  const coqAst = Monad.run(Program.compile(jsAst.program));
+
+  switch (coqAst.type) {
+    case "Error":
+      return Error.print(jsInput, coqAst.errors);
+    case "Success":
+      return doc.printer.printDocToString(Program.print(coqAst.value), {
+        printWidth,
+        tabWidth: 2,
+      }).formatted;
+    default:
+      return coqAst;
+  }
+}
