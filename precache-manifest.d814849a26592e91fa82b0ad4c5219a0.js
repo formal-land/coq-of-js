cb13f9b61e52499a3d82d8ae04a2125d988a800e
Guillaume Claret <dev@clarus.me>
2019-10-12 17:54:54 +0200

    Updates

---
+++ precache-manifest.d814849a26592e91fa82b0ad4c5219a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2d82a5df86056f9d900170678c0a58",
+    "url": "/coq-of-js/index.html"
+  },
+  {
+    "revision": "1d84f7f6bae25e9519f5",
+    "url": "/coq-of-js/static/css/main.27c515a0.chunk.css"
+  },
+  {
+    "revision": "8aabddee1388e2262640",
+    "url": "/coq-of-js/static/js/2.8b161179.chunk.js"
+  },
+  {
+    "revision": "1d84f7f6bae25e9519f5",
+    "url": "/coq-of-js/static/js/main.7213e745.chunk.js"
+  },
+  {
+    "revision": "962a9ec7732ab6fe0ffa",
+    "url": "/coq-of-js/static/js/runtime~main.5456ecdc.js"
+  }
+]);
